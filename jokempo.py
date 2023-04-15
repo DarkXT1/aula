0b53ad3a9097ef20c11c7be4a6973c9fce28ec3e
DarkXT1 <130846890+DarkXT1@users.noreply.github.com>
2023-04-15 11:50:34 -0300

    minha aula do ano passado, acho que algo deu errado, me dê dicas

---
+++ jokempo.py
@@ -0,0 +1,30 @@
+print ('insira seu nome:')
+nome = input()
+print("insira sua idade:")
+idade = int(input())
+if idade >= 18:
+        print(nome, 'você é adilto.')
+else:
+    print(nome, 'você ainda não é adulto.')
+
+print('qual a idade da primeira pessoa da equipe A')
+idade1 = int(input ())
+print('qual a idade da primeira pessoa da equipe B')
+idade2 = int(input ())
+print('qual a idade da segunda pessoa da equipe A')
+idade3 = int(input ())
+print('qual a idade da segunda pessoa da equipe B')	      
+idade4 = int(input ())
+A = idade1   idade3
+B = idade2   idade4
+if (A)>(B):
+        print("grupo A ganhou!")
+elif (A)<(B):
+        print("grupo B ganhou")
+else:
+        print("os dois empataram")
+
+nome1 = input ('informe o seu nome:')
+nome2 = input('informe o seu nome:')
+player1 = int(input('você tem 3 escolhas, 1 = pedra, 2 = tesoura e 3 = papel'))
+player2 = int(input('você tem 3 escolhas, 1 = pedra, 2 = tesoura e 3 = papel'))
